76cbd0e47a7b191df720bcfdc13d59c95aaa6035
matheusdantas16 <matheus.dantas160302@gmail.com>
2025-11-03 20:34:24 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Mercado Livre - Gerador de Lucros Automático</h1>
+      <p>Seu app Next.js está pronto para funcionar!</p>
+    </div>
+  );
+}
